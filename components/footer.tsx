5f68fdc554fb6727ae0b8a6be97c4009863ebaca
DigitalPotential <Carlos@digitalpotential.se>
2023-10-05 23:05:25 +0200

    Public

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2023{" "}
+                    <a
+                        href="https://www.carlosmanriquez.dev/"
+                        className="text-black hover:text-gray-500"
+                    >
+                        Carlos Manriquez
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
